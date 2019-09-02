e636222bc594beec469a7e1d49f6559ed7a437e6
Courtney Walker <cwalked@gmail.com>
2019-09-02 00:03:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 5
 second_number = 9
 sum = first_number+second_number
-sum = first_number-second_number
-sum = first_number*second_number
-sum = first_number/second_number
+difference = first_number-second_number
+quotient = first_number*second_number
+ = first_number/second_number
